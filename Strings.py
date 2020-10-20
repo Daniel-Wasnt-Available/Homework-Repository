807f1e365c5fbecf443a6feaadd26391f250666b
Daniel-Wasnt-Available <1wangdan5@hdsb.ca>
2020-10-20 14:31:30 -0400

    Strings homework

---
+++ Strings.py
@@ -0,0 +1,26 @@
+'''
+#made two because I wasn't sure which one the question was asking for
+word = "computers"
+print(len(word)) #prints total
+for i in range(len(word)):
+    print(i) # print every individuly number
+'''
+
+'''
+#questions 2
+word = "happy" [::-1]
+print(word)
+'''
+
+#still stuggling with this one however
+#4
+word = "radar"
+
+if word == (word [::1]):
+    print(word)
+    
+else:
+    print ("This is not a palindromes")
+
+
+    
